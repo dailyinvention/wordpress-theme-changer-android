177fec85accd1f3251de9c88b1171cf446a5f608
Stefan Holodnick <stefan@dailyinvention.com>
2013-03-21 21:40:37 -0400

    Got theme-titled buttons displaying using (XML-RPC) and made Github-friendly.

--- WordpressThemeChanger/src/com/dailyinvention/wordpressthemechanger/Client.java
+++ WordpressThemeChanger/src/com/dailyinvention/wordpressthemechanger/Client.java
@@ -2,15 +2,16 @@ package com.dailyinvention.wordpressthemechanger;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.HashMap;
-
 import org.xmlrpc.android.XMLRPCClient;
 import org.xmlrpc.android.XMLRPCException;
-
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 
 
@@ -20,31 +21,49 @@ public class Client extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_client);
+        //setContentView(R.layout.activity_client);
          
         //uri = URI.create("http://test-wp.apache.local/xmlrpc.php");
          
-      TextView TextViewtextView = (TextView) findViewById(R.id.text_view);
-      TextViewtextView.setText(getDataMethod());
-    }
+      findViewById(R.id.text_view);
+      LinearLayout linear = new LinearLayout(this);
+	  linear.setOrientation(LinearLayout.VERTICAL);       
+	  int count = getDataMethod().length;
+	  int i = 0;
+	  
+	  Button[] buttons = new Button[count];
+	  
+	  for(String key: getDataMethod()) {
+	  		buttons[i] = new Button(this);
+	  		buttons[i].setText(key);
+	  		linear.addView(buttons[i]);
+	  		++i;
+      }
+    	    setContentView(linear);
+      }
+      //TextViewtextView.setText(getDataMethod());
+    
      
-    private String getDataMethod() {
-        String text = "";
- 
+
+	private String[] getDataMethod() {
+        String[] keys = null;
+        
         try {
         	XMLRPCClient client = new XMLRPCClient(new URL("http://test-wp.apache.local/xmlrpc.php"));
             HashMap<String,Object> result = (HashMap<String,Object>) client.call("themes.getThemes","admin","password");
-            for (String key : result.keySet()){
-            	text = key;
-            }
+            //keys = new String[result.keySet().toArray().length];
+            Log.i("Count",Integer.toString(result.keySet().toArray().length));
+            keys = result.keySet().toArray(new String[result.keySet().toArray().length]);
             
         } catch (XMLRPCException e) {
             Log.w("XMLRPC Test", "Error", e);
-            text = "XMLRPC error" + e;
+            keys = new String[1];
+            keys[0] = "XMLRPC error" + e;
         } catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}       
-        return text;
+        //return text;
+        return keys;
     }
 }
