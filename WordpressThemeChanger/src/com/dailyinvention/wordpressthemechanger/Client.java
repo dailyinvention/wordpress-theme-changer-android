8061ca0f3835a988b03fd7a2274a395bf7dde78f
Stefan Holodnick <stefan@dailyinvention.com>
2013-03-25 21:25:20 -0400

    Now storing key (theme ID) and value (theme name) in 2d array.

--- WordpressThemeChanger/src/com/dailyinvention/wordpressthemechanger/Client.java
+++ WordpressThemeChanger/src/com/dailyinvention/wordpressthemechanger/Client.java
@@ -3,11 +3,15 @@ package com.dailyinvention.wordpressthemechanger;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 import org.xmlrpc.android.XMLRPCClient;
 import org.xmlrpc.android.XMLRPCException;
 
 import android.app.Activity;
+import android.graphics.LightingColorFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -15,9 +19,10 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 
 
-
 public class Client extends Activity {
-    
+	String url = (String)"http://test-wp.apache.local/xmlrpc.php";
+	String username = (String)"admin";
+	String password = (String)"password";
      
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,19 +34,29 @@ public class Client extends Activity {
       findViewById(R.id.text_view);
       LinearLayout linear = new LinearLayout(this);
 	  linear.setOrientation(LinearLayout.VERTICAL);       
-	  int count = getDataMethod().length;
+	  int count = getDataMethod(url, username, password).length;
 	  int i = 0;
 	  
-	  Button[] buttons = new Button[count];
+	  final Button[] buttons = new Button[count];
 	  
-	  for(final String key: getDataMethod()) {
+	  for(final String[] key: getDataMethod(url, username, password)) {
 	  		buttons[i] = new Button(this);
-	  		buttons[i].setText(key);
-	  		buttons[i].setOnClickListener(new View.OnClickListener() {
-
+	  		String active_theme = (String) getActiveTheme(url, username, password);
+	  		Log.i("Active Theme", key + ":" + active_theme);
+	  		if (key[1].equals(active_theme)) {
+	  			
+	  			buttons[i].getBackground().setColorFilter(new LightingColorFilter(0xFFFFFFFF, 0xDAA52000));
+	  		}
+	  		
+	  		buttons[i].setText(key[1]);
+	  		
+	  		final Object single_button = (Object) buttons[i];	
+	  		((View) single_button).setOnClickListener(new View.OnClickListener() {
+	  		
 	  	       @Override
-	  	       public void onClick(View linear) {
-	  	    	 switchTheme(key); 
+	  	       public void onClick(View linear) {  
+	  	    	 ((View) single_button).getBackground().setColorFilter(new LightingColorFilter(0xFFFFFFFF, 0xDAA52000));  
+	  	    	 switchTheme(key[0], url, username, password); 
 	  	       }
 	  	     });
 	  		linear.addView(buttons[i]);
@@ -50,11 +65,28 @@ public class Client extends Activity {
     	    setContentView(linear);
       }
       //TextViewtextView.setText(getDataMethod());
-    
-    private void switchTheme(String themeName) {
+    private String getActiveTheme(String url, String username, String password) {
+    	String theme_name = null;
+    	
     	try {
-			XMLRPCClient client = new XMLRPCClient(new URL("http://test-wp.apache.local/xmlrpc.php"));
-			String[] params = {"admin","password",themeName};
+			XMLRPCClient client = new XMLRPCClient(new URL(url));
+			theme_name = (String) client.call("themes.getActiveTheme",username, password);
+			
+    	} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (XMLRPCException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return theme_name;
+    		
+    }
+    
+    private void switchTheme(String themeName, String url, String username, String password) {
+    	try {
+			XMLRPCClient client = new XMLRPCClient(new URL(url));
+			String[] params = {username,password,themeName};
 			String result = (String) client.call("themes.switchThemes",params);
     	} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
@@ -67,20 +99,34 @@ public class Client extends Activity {
     	
     }
 
-	private String[] getDataMethod() {
-        String[] keys = null;
+	private String[][] getDataMethod(String url, String username, String password) {
+        String[][] keys = null;
         
         try {
-        	XMLRPCClient client = new XMLRPCClient(new URL("http://test-wp.apache.local/xmlrpc.php"));
-            HashMap<String,Object> result = (HashMap<String,Object>) client.call("themes.getThemes","admin","password");
+        	XMLRPCClient client = new XMLRPCClient(new URL(url));
+            HashMap<String,Object> result = (HashMap<String,Object>) client.call("themes.getThemes",username,password);
             //keys = new String[result.keySet().toArray().length];
-            Log.i("Count",Integer.toString(result.keySet().toArray().length));
-            keys = result.keySet().toArray(new String[result.keySet().toArray().length]);
+            //Log.i("Count",Integer.toString(result.values().toArray().length));
+            //keys = result.values().toArray(new String[result.values().toArray().length]);
+            String[][] arr = new String[result.size()][2];
+            Set entries = result.entrySet();
+            Iterator entriesIterator = entries.iterator();
             
+            int i = 0;
+            while(entriesIterator.hasNext()){
+
+                Map.Entry mapping = (Map.Entry) entriesIterator.next();
+
+                arr[i][0] = mapping.getKey().toString();
+                arr[i][1] = mapping.getValue().toString();
+
+                i++;
+            }
+            keys = arr;
         } catch (XMLRPCException e) {
             Log.w("XMLRPC Test", "Error", e);
-            keys = new String[1];
-            keys[0] = "XMLRPC error" + e;
+            keys = new String[1][1];
+            keys[0][1] = "XMLRPC error" + e;
         } catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
